6262b4e5203442da64b435c6a5f9f403c692b75b
Bongukwanda <workkwanda@gmail.com>
2026-03-26 17:58:39 +0200

    Revert "Update button layout code and fucntions"
    This reverts commit d7202e3ef72531e22f915c0fcb6a02b395e2d1a1.

--- src/services/button_controller.py
+++ src/services/button_controller.py
@@ -6,23 +6,81 @@ class ButtonController:
   
   def __init__(self):
     self.operation = Operations()
+    
+    self.equation = {
+      "numbers": [],
+      "function": ""
+    }
   
-  def add(self, equation: list) -> int:
-    resultant = sum(equation)
-    return resultant
+  # -------------------------------------
+  
+  def update_equation(self, equation_update, equation_value: str) -> None:
+    if equation_value == "numbers":
+      self.equation[equation_value].append(equation_update)
+    elif equation_value == "function":
+      self.equation[equation_value] = equation_update
+    else:
+      return
 
-class CommandBase(ABC):
-  def __init__(self):
-    self.controller = ButtonController()
+class EquationCommandBase(ABC):
+  def __init__(self, controller):
+    super().__init__()
+    self.controller = controller
   
   @abstractmethod
   def execute(self):
     ...
 
-class AddCommand(CommandBase):
-  def __init__(self, equation: list):
-    super().__init__()
-    self.equation = equation
+class EquationUpdateCommand(EquationCommandBase):
+  def __init__(self, controller, update, value):
+    super().__init__(controller=controller)
+    self.update = update
+    self.value = value
   
   def execute(self):
-    return self.controller.add(self.equation)
+    self.controller.update_equation(self.update, self.value)
+
+
+class CommandBase(ABC):
+  def __init__(self):
+    super().__init__()
+    self.ops = Operations()
+  
+  @abstractmethod
+  def execute(self):
+    ...
+
+class AdditionCommand(CommandBase):
+  def __init__(self):
+    super().__init__()
+
+  def execute(self, num_1, num_2):
+    return self.ops.add(num_1, num_2)
+
+class SubtractionCommand(CommandBase):
+  def __init__(self):
+    super().__init__()
+  
+  def execute(self, num_1, num_2):
+    return self.ops.subtract(num_1, num_2)
+
+class MultiplicationCommand(CommandBase):
+  def __init__(self):
+    super().__init__()
+  
+  def execute(self, num_1, num_2):
+    return self.ops.multiply(num_1, num_2)
+
+class DivideCommand(CommandBase):
+  def __init__(self):
+    super().__init__()
+  
+  def execute(self, num_1, num_2):
+    return self.ops.divide(num_1, num_2)
+
+class PowerOfCommand(CommandBase):
+  def __init__(self):
+    super().__init__()
+  
+  def execute(self, number, power_of):
+    return self.ops.power_of(number, power_of)
